9c76ff83c00c7e559d0efa61cbc85bc29b8d82ec
IRozelman <itamar.rozelman@gmail.com>
2025-09-22 18:04:26 +0300

    temporary reader header

--- lsm/ebpf_monitor/maps/syscall_events_user.h
+++ lsm/ebpf_monitor/maps/syscall_events_user.h
@@ -5,10 +5,13 @@
 #include <stdint.h>
 
 struct syscall_event_t {
+    uint64_t timestamp_ns;
     uint32_t pid;
     uint32_t ppid;
     uint32_t uid;
     uint32_t syscall_id;
+    uint32_t category;
+    char comm[16];  
     char  filename[256];
 };
 
